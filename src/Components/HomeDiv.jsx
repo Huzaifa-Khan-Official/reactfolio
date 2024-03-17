3efaaaf67fee650d48ba1428a88fcb78a20181e2
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-17 23:03:37 +0500

    homeDiv almost done

--- src/Components/HomeDiv.jsx
+++ src/Components/HomeDiv.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import bannerImg2 from '../assets/bannerImg2.svg'
-
+import homeDivImg from '../assets/homeDivImg.png'
 
 export default function HomeDiv() {
     return (
         <>
             <img src={bannerImg2} alt="" className='homeImg' />
             <div className="row px-4 py-5">
-                <div className="col-lg-6 col-md-12 col-12">
+                <div className="col-lg-6 col-md-12 col-12 d-flex flex-column justify-content-center align-items-center">
                     <div className="bubble2"></div>
                     <div className="bubble1"></div>
                     <div className="bubble3"></div>
@@ -30,6 +30,10 @@ export default function HomeDiv() {
                             hire me
                         </button>
                     </div>
+                </div>
+
+                <div className='col-lg-6 col-md-12 col-12'>
+                    <img src={homeDivImg} alt="" className='homeDivImg'/>
                 </div>
             </div>
         </>
