ed2a55dd2a8b45dbec26b0ad0f3e3c9d689783b8
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-19 23:56:25 +0500

    still working

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import skillsImg from '../assets/skills.png'
 
 export default function Skills() {
-  return (
-    <div className='skillsDiv row px-4 py-5'>
-        <h1>Skills</h1>
+    return (
+        <div className='row px-4 py-5'>
+            <div className='col-lg-6 col-md-12 col-12 d-flex flex-column justify-content-center align-items-center'>
+                <img src={skillsImg} alt="" className='skillsImg' />
+            </div>
+            <div className='col-lg-6 col-md-12 col-12 d-flex flex-column justify-content-center align-items-center'>
+                <h1 className='skillsHeading'>
+                    What I do
+                </h1>
+                <p className='skillsSubTitle'>
 
-    </div>
-  )
+                </p>
+            </div>
+        </div>
+    )
 }
