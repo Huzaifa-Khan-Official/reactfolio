3105b236d928a676cef72ddc5bc9f42141d7ccf9
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-24 12:52:45 +0500

    working on image carousel

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ImageCarousel from './ImageCarousel'
 
 export default function Portfolio() {
     return (
         <div>
             portfolio
+            <ImageCarousel />
         </div>
     )
 }
