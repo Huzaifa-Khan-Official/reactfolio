80dfd3d92245e151a11d564ac405af39ccebcb74
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-27 20:26:23 +0500

    still wroking

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { LuPhoneCall } from "react-icons/lu";
 import { BsChatLeftText } from "react-icons/bs";
+import { CiLocationOn } from "react-icons/ci";
 
 export default function Contact() {
     return (
         <div className='row'>
             <div className="col-lg-5 col-md-12">
-                <div className="getInTouchDiv mb-5">
+                <div className="getInTouchDiv">
                     <h6>
                         Get in Touch
                     </h6>
@@ -15,7 +16,7 @@ export default function Contact() {
                     </h2>
                 </div>
 
-                <div className="media d-flex gap-3">
+                <div className="media d-flex gap-3 align-items-center">
                     <LuPhoneCall className='mediaIcon' />
                     <div className="mediaBody">
                         <h6>
@@ -27,7 +28,7 @@ export default function Contact() {
                     </div>
                 </div>
 
-                <div className="media d-flex gap-3">
+                <div className="media d-flex gap-3 align-items-center">
                     <BsChatLeftText className='mediaIcon' />
                     <div className="mediaBody">
                         <h6>
@@ -37,6 +38,48 @@ export default function Contact() {
                             john@gmail.com
                         </h4>
                     </div>
+                </div>
+
+
+                <div className="media d-flex gap-3 align-items-center">
+                    <CiLocationOn className='mediaIcon' />
+                    <div className="mediaBody">
+                        <h6>
+                            Get Me Here
+                        </h6>
+                        <h4>
+                            Z105 - London, UK
+                        </h4>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-lg-7 col-md-12">
+                <div className="getInTouchDiv">
+                    <h6>
+                        Contact us
+                    </h6>
+                    <h2>
+                        Let me know here Know Here
+                    </h2>
+                </div>
+
+                <div className="inputDiv">
+                    <div className="row">
+                        <div className="col-6">
+                            <input type="text" placeholder='Your Name*' className='form-control'/>
+                        </div>
+                        <div className="col-6">
+                            <input type="email" placeholder='Your Email*' className='form-control'/>
+                        </div>
+                        <div className="col-6">
+                            <input type="text" placeholder='Subject*' className='form-control'/>
+                        </div>
+                        <div className="col-6">
+                            <input type="text" placeholder='Phone*' className='form-control'/>
+                        </div>
+                    </div>
+                    <textarea rows="6" className='form-control textArea'></textarea>
                 </div>
             </div>
         </div>
