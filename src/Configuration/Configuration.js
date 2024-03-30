bf68839ece2b50099d4a5988a5377497fbf67ef4
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-03-30 20:15:32 +0500

    start working on receiving query message

---
+++ src/Configuration/Configuration.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAedqKe1ryAEcZfFMUTHA_MLqlKXcp9jV8",
+    authDomain: "huzaifa-reacfolio.firebaseapp.com",
+    projectId: "huzaifa-reacfolio",
+    storageBucket: "huzaifa-reacfolio.appspot.com",
+    messagingSenderId: "286901249528",
+    appId: "1:286901249528:web:cd7ce2d719f9c16d00a20d",
+    measurementId: "G-LMME11M7TW"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const messaging = getMessaging(app);
+
+
+
+export { messaging }
